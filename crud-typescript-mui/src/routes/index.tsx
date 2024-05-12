86e301ef47882dc0b6512142dab34c324e3d2895
DBDev90 <bordinassidouglas@gmail.com>
2024-05-12 11:49:02 -0300

    feat: adicionado projeto crud

---
+++ crud-typescript-mui/src/routes/index.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@mui/material";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { useDrawerContext } from "../shared/contexts";
+
+export const AppRoutes = () => {
+  const { toggleDrawerOpen } = useDrawerContext();
+
+  return (
+    <Routes>
+      <Route
+        path="/pagina-inicial"
+        element={
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={toggleDrawerOpen}
+          >
+            Teste
+          </Button>
+        }
+      />
+
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+    </Routes>
+  );
+};
