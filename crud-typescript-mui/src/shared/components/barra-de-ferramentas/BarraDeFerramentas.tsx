269ecb108936c034e66f835d3b77c9e0ce050fec
DBDev90 <bordinassidouglas@gmail.com>
2024-05-13 11:19:58 -0300

    feat: barra de ferramentas da listagem

---
+++ crud-typescript-mui/src/shared/components/barra-de-ferramentas/BarraDeFerramentas.tsx
@@ -0,0 +1,71 @@
+import {
+    Box,
+    Button,
+    Icon,
+    InputAdornment,
+    Paper,
+    TextField,
+    useTheme,
+} from "@mui/material";
+
+interface IBarraDeFerramentasProps {
+    textoBusca?: string;
+    mostrarInputBusca?: boolean;
+    aoMudarTextoBusca?: (novoTexto: string) => void;
+
+    textoBotaoNovo?: string;
+    mostrarBotaoNovo?: boolean;
+    aoClicarEmNovo?: () => void;
+}
+
+export const BarraDeFerramentas: React.FC<IBarraDeFerramentasProps> = ({
+    textoBusca = '',
+    mostrarInputBusca = false,
+    aoMudarTextoBusca: aoMudarTextBusca,
+    textoBotaoNovo = 'Novo',
+    mostrarBotaoNovo = true,
+    aoClicarEmNovo
+}) => {
+    const theme = useTheme();
+
+    return (
+        <Box
+            gap={1}
+            marginX={1}
+            padding={1}
+            display="flex"
+            component={Paper}
+            alignItems="center"
+            height={theme.spacing(5)}
+        >
+            {mostrarInputBusca && (
+                <TextField
+                    size="small"
+                    value={textoBusca}
+                    placeholder="Pesquisar..."
+                    onChange={(e) => aoMudarTextBusca?.(e.target.value)}
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <Icon>search</Icon>
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+            )}
+            <Box flex={1} display="flex" justifyContent="end">
+                {mostrarBotaoNovo && (
+                    <Button
+                        color="primary"
+                        disableElevation
+                        variant="contained"
+                        onClick={aoClicarEmNovo}
+                        endIcon={<Icon>add</Icon>}
+                    >
+                        {textoBotaoNovo}
+                    </Button>
+                )}
+            </Box>
+        </Box>
+    );
+};
