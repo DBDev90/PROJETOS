f2e220c28ccdc020178254ebd1f77f5c6acc0467
DBDev90 <bordinassidouglas@gmail.com>
2024-05-13 09:43:07 -0300

    feat: layout base responsivo

--- crud-typescript-mui/src/shared/layouts/LayoutBaseDePagina.tsx
+++ crud-typescript-mui/src/shared/layouts/LayoutBaseDePagina.tsx
@@ -1,31 +1,35 @@
 import { Box, Icon, IconButton, Theme, Typography, useMediaQuery, useTheme } from "@mui/material";
 import { useDrawerContext } from "../contexts";
+import { ReactNode } from "react";
 
 interface ILayoutBaseDePaginaProps {
     titulo: string;
+    barraDeFerramentas?: ReactNode;
     children: React.ReactNode;
 }
 
-export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({ children, titulo }) => {
+export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({ children, titulo, barraDeFerramentas }) => {
     const smDown = useMediaQuery((theme: Theme) => theme.breakpoints.down("sm"));
+    const mdDown = useMediaQuery((theme: Theme) => theme.breakpoints.down("md"));
     const theme = useTheme();
     const { toggleDrawerOpen } = useDrawerContext()
 
     return (
         <Box height="100%" display="flex" flexDirection="column" gap={1}>
-            <Box display="flex" alignItems="center" padding={1} height={theme.spacing(12)} gap={1}>
+            <Box display="flex" alignItems="center" padding={1} height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)} gap={1}>
                 {smDown && (<IconButton onClick={toggleDrawerOpen}>
                     <Icon>menu</Icon>
                 </IconButton>)}
 
-                <Typography variant="h5">
+                <Typography variant={smDown ? "h5" : mdDown ? "h4" : "h3"} whiteSpace="nowrap" overflow="hidden" textOverflow="ellipsis">
                     {titulo}
                 </Typography>
             </Box>
-            <Box>
-                Barra de ferramentas
-            </Box>
-            <Box>
+            {barraDeFerramentas && (
+                <Box>
+                    {barraDeFerramentas}
+                </Box>)}
+            <Box flex={1} overflow="auto">
                 {children}
             </Box>
         </Box>
