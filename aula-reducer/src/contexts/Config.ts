1b64e9cd798815e73ce8df64624b7b6ccaebe622
DBDev90 <bordinassidouglas@gmail.com>
2024-06-06 13:34:26 -0300

    feat: adicionando reducer de usuario

--- aula-reducer/src/contexts/Config.ts
+++ aula-reducer/src/contexts/Config.ts
@@ -1,16 +1,20 @@
 import { ThemeAction } from "../types/ThemeReducer";
+import { UserAction } from "../types/UserReducer";
 import { ThemeReducer, themeInitialState } from "./reducers/ThemeReducer";
+import { userInitialState, userReducer } from "./reducers/UserReducer";
 
 //Exporting all initial states
 export const InitialState = {
     theme: themeInitialState,
+    user: userInitialState,
 };
 
 //Exporting all reducers
 export const Reducers = (state: InitialStateType, action: ActionType) => ({
     theme: ThemeReducer(state.theme, action),
+    user: userReducer(state.user, action)
 });
 
 //Exporting all types
 export type InitialStateType = typeof InitialState;
-export type ActionType = ThemeAction;
+export type ActionType = ThemeAction | UserAction;
---
+++ aula-reducer/src/contexts/reducers/UserReducer.ts
@@ -0,0 +1,18 @@
+import { UserState } from "../../types/UserReducer";
+import { ActionType } from "../Config";
+
+export const userInitialState: UserState = {
+    user: null,
+};
+
+export const userReducer = (state: UserState, action: ActionType): UserState => {
+    switch (action.type) {
+        case "SIGNIN":
+            return action.payload;
+        case "SIGNOUT":
+            return userInitialState;
+        default:
+            return state;
+
+    }
+};
