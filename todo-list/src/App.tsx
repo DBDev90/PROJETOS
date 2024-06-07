9cf9577c7d8805b0b17582bef03f8bcc51042e35
DBDev90 <bordinassidouglas@gmail.com>
2024-06-07 11:40:13 -0300

    refactor: utilizando css module no App.tsx

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,10 +1,16 @@
 import { useSaveTodos } from "./hooks/useSaveTodos";
+import styles from "./App.module.css";
+import { Header } from "./components/Header";
 
 export const App = () => {
   // Monitoring all changes on todos and save in localStorage
   useSaveTodos();
 
   return (
-    <></>
+    <div className={styles.container}>
+      <header>
+        <Header />
+      </header>
+    </div>
   );
 }
