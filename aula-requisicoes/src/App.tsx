b8abf324dcae4606ff292018d4f8a341154ee039
DBDev90 <bordinassidouglas@gmail.com>
2024-06-10 16:15:09 -0300

    feat: tipando requisicao com axios

--- aula-requisicoes/src/App.tsx
+++ aula-requisicoes/src/App.tsx
@@ -1,8 +1,15 @@
 import axios from "axios";
 
+type Post = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+}
+
 export const App = () => {
   const handleGetPosts = () => {
-    axios.get("https://jsonplaceholder.typicode.com/posts")
+    axios.get<Post[]>("https://jsonplaceholder.typicode.com/posts")
       .then(response => { console.log(response.data); });
   }
 
