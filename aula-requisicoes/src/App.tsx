7f8c8c4fb8568842851b49cdcd09c75634ef29c5
DBDev90 <bordinassidouglas@gmail.com>
2024-06-10 11:24:30 -0300

    feat: exibindo dados do get

--- aula-requisicoes/src/App.tsx
+++ aula-requisicoes/src/App.tsx
@@ -1,22 +1,39 @@
+import { useState } from "react";
+
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
 
 export const App = () => {
+  const [postsData, setPostsData] = useState<Post[]>([])
+
   const handleGetPosts = async () => {
-    const posts = fetch("https://jsonplaceholder.typicode.com/posts", {
+    const request = await fetch("https://jsonplaceholder.typicode.com/posts", {
       method: "GET",
       headers: {
         "Content-type": "application/json"
       }
-    }).then(response => response.json())
+    });
 
-    // const postsJson = await posts.json();
-    const postsAwait = await posts
+    const posts: Post[] = await request.json();
 
-    console.log(postsAwait);
+    setPostsData(posts);
   }
 
   return (
     <div>
       <button onClick={handleGetPosts}>Fazer requisição</button>
+
+      <ul>
+        {postsData.map((item) => (
+          <li key={item.id}>
+            {item.title}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
