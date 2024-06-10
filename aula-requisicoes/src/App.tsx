eb80a3c94fd07e0e632e77a5ce8240f6d17603f1
DBDev90 <bordinassidouglas@gmail.com>
2024-06-10 11:12:26 -0300

    feat: fazendo requisicao get

--- aula-requisicoes/src/App.tsx
+++ aula-requisicoes/src/App.tsx
@@ -1,8 +1,22 @@
 
 export const App = () => {
+  const handleGetPosts = async () => {
+    const posts = fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json"
+      }
+    }).then(response => response.json())
+
+    // const postsJson = await posts.json();
+    const postsAwait = await posts
+
+    console.log(postsAwait);
+  }
+
   return (
     <div>
-
+      <button onClick={handleGetPosts}>Fazer requisição</button>
     </div>
   );
 }
