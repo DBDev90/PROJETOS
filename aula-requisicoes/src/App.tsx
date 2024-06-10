c6b11c46cc1f8c3f5e2273c73d69be9b5abd98ab
DBDev90 <bordinassidouglas@gmail.com>
2024-06-10 16:12:46 -0300

    feat: utilizando axios

--- aula-requisicoes/src/App.tsx
+++ aula-requisicoes/src/App.tsx
@@ -1,94 +1,14 @@
-// import { useState } from "react";
-
-// type Post = {
-//   userId: number;
-//   id: number;
-//   title: string;
-//   body: string;
-// };
-
-// export const App = () => {
-//   const [postsData, setPostsData] = useState<Post[]>([]);
-//   const [loading, setLoading] = useState(false);
-//   const [errorMessage, setErrorMessage] = useState("");
-
-//   const handleGetPosts = async () => {
-//     setLoading(true);
-
-//     try {
-//       const request = await fetch("https://jsonplaceholder.typicode.com/posts", {
-//         method: "GET",
-//         headers: {
-//           "Content-type": "application/json"
-//         }
-//       });
-//       const posts: Post[] = await request.json();
-//       setPostsData(posts);
-//     } catch (error) {
-//       setErrorMessage("Erro ao consultar os dados.")
-//     }
-
-//     setLoading(false);
-//   };
-
-//   return (
-//     <div>
-//       <button onClick={handleGetPosts}>Fazer requisição</button>
-
-//       {loading && "Carregando..."}
-//       {errorMessage && (
-//         <p>{errorMessage}</p>
-//       )}
-
-//       <ul>
-//         {postsData.map((item) => (
-//           <li key={item.id}>
-//             {item.title}
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
+import axios from "axios";
 
 export const App = () => {
-  const handlePostPosts = async () => {
-    const request = await fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      body: JSON.stringify({
-        title: "Novo post",
-        body: "Body post",
-        userId: 1
-      }),
-      headers: {
-        "Content-type": "appplication/json"
-      }
-    });
-
-    const result = await request.json();
-    console.log(result);
-  };
-
-  const handlePutPosts = async () => {
-    const request = await fetch("https://jsonplaceholder.typicode.com/posts/1", {
-      method: "PUT",
-      body: JSON.stringify({
-        title: "Novo post PUT",
-        body: "Body post"
-      }),
-      headers: {
-        "Content-type": "appplication/json"
-      }
-    });
-
-    const result = await request.json();
-    console.log(`O resultado foi: ${result}`);
-  };
+  const handleGetPosts = () => {
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then(response => { console.log(response.data); });
+  }
 
   return (
     <div>
-      <button onClick={handlePostPosts}>Fazer requisição POST</button>
-      <button onClick={handlePutPosts}>Fazer requisição PUT</button>
+      <button onClick={handleGetPosts}>Fazer request</button>
     </div>
   );
-};
+}
