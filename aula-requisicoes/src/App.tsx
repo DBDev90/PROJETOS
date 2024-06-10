397839104f87969730882a2242629ae17588fb7b
DBDev90 <bordinassidouglas@gmail.com>
2024-06-10 16:22:42 -0300

    feat: enviando body e params

--- aula-requisicoes/src/App.tsx
+++ aula-requisicoes/src/App.tsx
@@ -11,11 +11,20 @@ export const App = () => {
   const handleGetPosts = () => {
     axios.get<Post[]>("https://jsonplaceholder.typicode.com/posts")
       .then(response => { console.log(response.data); });
-  }
+  };
+
+  const handlePostPosts = () => {
+    axios.post("https://jsonplaceholder.typicode.com/posts", {
+      title: "Novo post",
+      body: "Novo body",
+      userId: 1
+    })
+  };
 
   return (
     <div>
       <button onClick={handleGetPosts}>Fazer request</button>
+      <button onClick={handlePostPosts}>Fazer request POST</button>
     </div>
   );
 }
