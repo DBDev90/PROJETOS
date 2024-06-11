c45787b27feb5855e87e4e5680b5f14fd2ed9f3d
DBDev90 <bordinassidouglas@gmail.com>
2024-06-11 10:58:58 -0300

    feat: lidando com parametros de consulta

--- aula-react-router/src/pages/Products.tsx
+++ aula-react-router/src/pages/Products.tsx
@@ -1,6 +1,21 @@
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 export const Products = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        // setSearchParams({ count: String(count) });
+
+        // searchParams.set("count", String(count));
+        // setSearchParams(searchParams);
+
+        console.log(searchParams.get("parametro-2"));
+    }, [count, searchParams, setSearchParams]);
+
+    const handleClickCount = () => setCount(count + 1);
+
     const navigate = useNavigate();
 
     const handleClick = () => {
@@ -11,7 +26,9 @@ export const Products = () => {
         <div>
             <h5>Essa é a página de Produtos</h5>
 
-            <button onClick={handleClick}>Ir para um produto</button>
+            <button onClick={handleClickCount} style={{ width: "100%" }}>Adicionar + 1</button>
+            <br />
+            <button onClick={handleClick} style={{ width: "100%" }}>Ir para um produto</button>
         </div>
     );
 };
