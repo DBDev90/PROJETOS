eed81e50b8ea0ea86aa2e6fdb7a5c38a0a189273
DBDev90 <bordinassidouglas@gmail.com>
2024-06-11 16:27:30 -0300

    feat: criando o hook useAuth

--- dashboard/src/redux/slices/authSlice.ts
+++ dashboard/src/redux/slices/authSlice.ts
@@ -23,12 +23,12 @@ export const authSlice = createSlice({
         setAuthToken: (state, action: PayloadAction<IAuthState["authToken"]>) => {
             state.authToken = action.payload;
         },
-        setAuthState: (state, action: PayloadAction<IAuthState["authStatus"]>) => {
+        setAuthStatus: (state, action: PayloadAction<IAuthState["authStatus"]>) => {
             state.authStatus = action.payload;
         },
     },
 });
 
-export const { setUser, setAuthToken, setAuthState } = authSlice.actions;
+export const { setUser, setAuthToken, setAuthStatus } = authSlice.actions;
 
 export default authSlice.reducer;
