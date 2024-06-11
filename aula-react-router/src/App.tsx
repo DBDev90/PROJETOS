6c8bd16f5f37c39ec4c98a6fef632e1a4f872efc
DBDev90 <bordinassidouglas@gmail.com>
2024-06-11 09:26:40 -0300

    feat: trabalhando com layouts nas rotas

--- aula-react-router/src/App.tsx
+++ aula-react-router/src/App.tsx
@@ -1,15 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import { About, Home, Product, Products } from "./pages";
+import { MainLayout } from "./components";
 
 export const App = () => {
   return (
     <div>
       <Routes>
-        <Route index element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products">
-          <Route index element={<Products />} />
-          <Route path=":cateforyId/:productId" element={<Product />} />
+        <Route element={<MainLayout />}>
+
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products">
+            <Route index element={<Products />} />
+            <Route path=":cateforyId/:productId" element={<Product />} />
+          </Route>
         </Route>
       </Routes>
     </div>
