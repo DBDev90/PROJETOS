efb3983bc0a9029ba4b3bd3ea6c34b45e48abea4
DBDev90 <bordinassidouglas@gmail.com>
2024-06-11 11:39:20 -0300

    feat: organizando rotas

--- aula-react-router/src/App.tsx
+++ aula-react-router/src/App.tsx
@@ -1,31 +1,10 @@
-import { Outlet, Route, Routes } from "react-router-dom";
-import { About, Home, Login, NotFoundPage, Product, Products } from "./pages";
-import { MainLayout } from "./components";
-import { AuthMiddleware } from "./middlewares/AuthMiddleware";
+import { AppRoutes } from "./routes/AppRoutes";
+
 
 export const App = () => {
   return (
     <div>
-      <Routes>
-        <Route path="*" element={<NotFoundPage />} />
-
-        <Route element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route
-            path="/products"
-            element={
-              <AuthMiddleware>
-                <Outlet />
-              </AuthMiddleware>
-            }
-          >
-            <Route index element={<Products />} />
-            <Route path=":cateforyId/:productId" element={<Product />} />
-          </Route>
-          <Route path="/login" element={<Login />} />
-        </Route>
-      </Routes>
+      <AppRoutes />
     </div>
   );
 };
---
+++ aula-react-router/src/routes/AppRoutes.tsx
@@ -0,0 +1,29 @@
+import { Outlet, Route, Routes } from "react-router-dom";
+import { About, Home, Login, NotFoundPage, Product, Products } from "../pages";
+import { MainLayout } from "../components";
+import { AuthMiddleware } from "../middlewares/AuthMiddleware";
+
+export const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="*" element={<NotFoundPage />} />
+
+            <Route element={<MainLayout />}>
+                <Route index element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route
+                    path="/products"
+                    element={
+                        <AuthMiddleware>
+                            <Outlet />
+                        </AuthMiddleware>
+                    }
+                >
+                    <Route index element={<Products />} />
+                    <Route path=":cateforyId/:productId" element={<Product />} />
+                </Route>
+                <Route path="/login" element={<Login />} />
+            </Route>
+        </Routes>
+    );
+}
