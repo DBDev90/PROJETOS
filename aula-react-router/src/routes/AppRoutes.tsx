d3a3796acd87e384f19f56c39b3821c47a5b110e
DBDev90 <bordinassidouglas@gmail.com>
2024-06-11 11:49:11 -0300

    feat: outra forma de criar rotas

--- aula-react-router/src/routes/AppRoutes.tsx
+++ aula-react-router/src/routes/AppRoutes.tsx
@@ -1,8 +1,9 @@
-import { Outlet, Route, Routes } from "react-router-dom";
-import { About, Home, Login, NotFoundPage, Product, Products } from "../pages";
-import { MainLayout } from "../components";
+import { Outlet, useRoutes } from "react-router-dom";
 import { AuthMiddleware } from "../middlewares/AuthMiddleware";
+import { About, Home, Login, NotFoundPage, Product, Products } from "../pages";
 
+//Metodo 1
+/*
 export const AppRoutes = () => {
     return (
         <Routes>
@@ -26,4 +27,43 @@ export const AppRoutes = () => {
             </Route>
         </Routes>
     );
+}
+*/
+
+//Método 2
+export const AppRoutes = () => {
+    const element = useRoutes([
+        {
+            index: true,
+            element: <Home />
+        },
+        {
+            path: "/about",
+            element: <About />
+        },
+        {
+            path: "/products",
+            element: <AuthMiddleware><Outlet /></AuthMiddleware>,
+            children: [
+                {
+                    index: true,
+                    element: <Products />
+                },
+                {
+                    path: ":categoryId/>productId",
+                    element: <Product />
+                }
+            ]
+        },
+        {
+            path: "/login",
+            element: <Login />
+        },
+        {
+            path: "*",
+            element: <NotFoundPage />
+        }
+    ]);
+
+    return element;
 }
