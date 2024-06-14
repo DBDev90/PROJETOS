a85460b44f18001b5b69cebd63fc43c2fac249b3
DBDev90 <bordinassidouglas@gmail.com>
2024-06-14 14:04:24 -0300

    feat: criando tela nova transacao

--- dashboard/src/routes/index.tsx
+++ dashboard/src/routes/index.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import { Auth } from "../pages/auth"
 import { Layout } from "../components/layout"
 import { Home } from "../pages/home"
+import { NewTransaction } from "../pages/transaction/new"
 
 export const MainRoutes = () => {
     return (
@@ -18,6 +19,10 @@ export const MainRoutes = () => {
 
             <Route element={<Layout />}>
                 <Route index element={<Home />} />
+
+                <Route path="/transacoes">
+                    <Route path="nova" element={<NewTransaction />} />
+                </Route>
             </Route>
         </Routes>
     )
